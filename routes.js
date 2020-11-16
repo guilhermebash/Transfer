43cda832b1997d2d404b9fc9fc890691002e0ecc
Guilherme <guilhermefreitasdsi@gmail.com>
2020-11-16 14:09:27 -0300

    Redirecionando rota / para a tela de login

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const AuthController = require('./src/controllers/AuthController');
 const DashboardController = require('./src/controllers/DashboardController');
 
+routes.get('/', (req,res) => {res.redirect('/login')});
 routes.get('/login', AuthController.get);
 routes.post('/login', AuthController.post);
 
